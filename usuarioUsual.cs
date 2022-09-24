79e6429f897bf0cef61556b71255d7c719dd025b
danielarcil4 <daniel.arcila@udea.edu.co>
2022-09-24 08:51:50 -0500

    Se agrego la clase usuario

---
+++ usuarioUsual.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoParqueadero
+{
+    public class usuarioUsual:Usuario
+    {
+        string _nombreUsuario="";
+        string _contrasenia="";
+
+        public usuarioUsual(string nombreUsuario, string contrasenia)
+        {
+            NombreUsuario = nombreUsuario;
+            Contrasenia = contrasenia;
+        }
+
+        public override string NombreUsuario { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string Contrasenia { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
